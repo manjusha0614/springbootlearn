815a9606b69deb093c73c9c9d6b78b25fe8b3247
Rashmi Dudipalla <dudipallamanjusha@gmail.com>
2025-08-20 17:17:27 -0500

    task_2_odd_or_even

---
+++ task_2_odd_or_even/src/test/java/com/manju/task_2_odd_or_even/Task2OddOrEvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manju.task_2_odd_or_even;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task2OddOrEvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
