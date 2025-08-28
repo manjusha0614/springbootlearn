682c6a7651f27ec4f8e5a37845abeced8bf15472
Rashmi Dudipalla <dudipallamanjusha@gmail.com>
2025-08-28 11:21:22 -0500

    countdigits

---
+++ task_019_count_digits/src/main/java/com/manju/task_019_count_digits/Task019CountDigitsApplication.java
@@ -0,0 +1,13 @@
+package com.manju.task_019_count_digits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task019CountDigitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task019CountDigitsApplication.class, args);
+	}
+
+}
