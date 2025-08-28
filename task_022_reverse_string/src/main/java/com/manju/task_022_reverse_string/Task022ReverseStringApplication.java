114dcac3925d006007ced5f7afc66c3e12ab710c
Rashmi Dudipalla <dudipallamanjusha@gmail.com>
2025-08-28 12:01:16 -0500

    stringreverse

---
+++ task_022_reverse_string/src/main/java/com/manju/task_022_reverse_string/Task022ReverseStringApplication.java
@@ -0,0 +1,13 @@
+package com.manju.task_022_reverse_string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task022ReverseStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task022ReverseStringApplication.class, args);
+	}
+
+}
---
+++ task_022_reverse_string/src/test/java/com/manju/task_022_reverse_string/Task022ReverseStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manju.task_022_reverse_string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task022ReverseStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
