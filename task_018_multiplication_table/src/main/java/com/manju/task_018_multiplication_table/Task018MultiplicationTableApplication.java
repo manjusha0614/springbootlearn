0e41e3f9cc31ac1744120f4c0cae9ba6bac65f3e
Rashmi Dudipalla <dudipallamanjusha@gmail.com>
2025-08-28 11:12:14 -0500

    multiplicationofanumber

---
+++ task_018_multiplication_table/src/main/java/com/manju/task_018_multiplication_table/Task018MultiplicationTableApplication.java
@@ -0,0 +1,13 @@
+package com.manju.task_018_multiplication_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task018MultiplicationTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task018MultiplicationTableApplication.class, args);
+	}
+
+}
---
+++ task_018_multiplication_table/src/test/java/com/manju/task_018_multiplication_table/Task018MultiplicationTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manju.task_018_multiplication_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task018MultiplicationTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
